a0dfc2204bec68b808ebe87863a91092abc76008
Maximilian Alba <albam476@gmail.com>
2023-03-21 19:26:34 -0400

    commit tree 2d1014184fc7dc7ff45862372579143e3623c25e

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -9,9 +11,11 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
+    bird = Bird.create!(bird_params)
+      render json: bird, status: :created
   end
+
+  
 
   # GET /birds/:id
   def show
@@ -22,7 +26,7 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
   end
 
@@ -34,6 +38,11 @@ class BirdsController < ApplicationController
   end
 
   private
+
+  
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 
   def find_bird
     Bird.find(params[:id])
